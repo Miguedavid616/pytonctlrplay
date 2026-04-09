e02837ec8793807914c19a3fdb0306a3423fc9b1
WendellCS <wendellcs131@gmail.com>
2026-04-09 18:30:09 -0300

    first commit

---
+++ aula2.py
@@ -0,0 +1,79 @@
+# idade = input('idade')
+# print('sua idade e:', idade)
+# parte_1 = 'daniel' 
+# parte_2 = "video maker"
+# frase = parte_1 + parte_2 + "!"
+# print(frase)
+# frase = input("digite uma frase")
+# print(frase.count("i"))
+# print(frase.count('a'))
+# print(frase.count('e'))
+# print(frase.count("o")) 
+
+# Listas
+
+#lista_de_jogos = ['honkai star rail',"genshin impact",'punishing gray rave']
+#print(lista_de_jogos)
+
+#jogo_1 = input('diga um nome de um jogo')
+#lista_de_jogos.append(jogo_1)
+#print(lista_de_jogos)
+#print(lista_de_jogos) 
+#if idade >= 18:
+
+ #   print("voce tem 18 anos ou mais")
+#    print("voce e menor da idade")
+# numero = int(input("digite um numero"))
+# if numero > 0:
+#       print('seu numero e posivo')
+# elif  numero < 0:
+#     print('seu numero e negativo')
+# else:
+#      print('seu numero e 0')
+# numero = int(input('insira o numero'))
+#if numero > 1:
+#    print('seu numero e maior que 1 ')
+#elif numero < 1:
+#    print('seu numero e maior que 1')
+#else:
+ #   print('seu numero e 1') 
+#for i in range(0, 11):
+#     print(i)
+#lista_de_jogos = ['dragon ball', 'naruto', 'one piece', 'king', 'fatal fury', 'art of fighters', 'honkai', 'genshin']
+#print(lista_de_jogos)
+#for intem in lista_de_jogos:
+#  print(intem)
+#genshin = {
+  #  'reacao': ['derretimento'],
+  #  'personagen': 'furina',
+  #  'espada': 'espada de favonius',
+  #  'artefato': 'trupe dourada',
+  #  'status': 'vida',
+
+
+#}
+#genshin['reacao'] = 'vaporizacao'
+#genshin['talento'] = 10
+#print(genshin)
+
+#def seu_jogo(nome):
+    #  print('seu jogo favorito e´´ ' + nome)
+#seu_jogo('genshin impact')
+#seu_jogo('resident evil')
+#seu_jogo('fatal fury')
+#def multiplicar(a , b):
+#    print(a*b)
+#multiplicar(10 , 5)
+#def dividir(a , b):
+#    print(a/b)
+#dividir(300 , 20)
+#def subtracao(a , b):
+ #   print(a-b)
+#subtracao(77 , 10)
+#def multiplicar(a):
+#   return a*2
+#resultado = multiplicar(2)
+
+
+  
+
